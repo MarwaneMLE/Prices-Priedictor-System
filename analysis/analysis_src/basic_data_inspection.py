d48e078e3be6f6f1f60ed2ac41d5f2b5e2244e62
ubuntu <MarwaneMLE>
2024-09-22 09:01:16 +0100

    Data inspection

---
+++ analysis/analysis_src/basic_data_inspection.py
@@ -0,0 +1,111 @@
+from abc import ABC, abstractmethod
+import pandas as pd
+
+
+
+class DataInspectionStrategy(ABC):
+    """
+    Abstract class for Data Inspection
+    """
+    def inspect(self, df:pd.DataFrame):
+        """
+        inspect method to perform specific type of data inspection
+
+        Args:
+            df: dtaframe to inspect
+
+        Returns: 
+            None: this method print the inspection results
+        """
+        pass
+
+
+class DataTypesInspectionStrategy(DataInspectionStrategy):
+    """
+    Abstract class for Data Inspection
+    """
+    def inspect(self, df:pd.DataFrame):
+        """
+        inspect method to perform specific type of data inspection
+
+        Args:
+            df(pd.DataFrame): dataframe to inspect
+
+        Returns: 
+            None: prints data Types and Non-null Counts
+        """
+        print("\n Data Types and Non-null Counts:")
+        print(df.info())
+
+
+class SummaryStatisticsInspectionStrategy(DataInspectionStrategy):
+    """
+    To check descriptive statistics
+    """
+    def inspect(self, df:pd.DataFrame):
+        """
+        Prints summary statistics for numerical and categoricaal data
+        Args:
+            df(pd.DataFrame): dataframe to inspect
+
+        Returns: 
+            None: prints summary statistics
+        """
+        print("\n Summary Statistics of (Numerical Features):")
+        print(df.describe())
+
+        print("\n Summary Statistics of (Categorical Features):")
+        print(df.describe(include=["object"]))
+
+
+class DataInspector:
+    def __init__(self, strategy: DataInspectionStrategy):
+        """
+        Initializes the DataInspector with a specific inspection
+        Args:
+            strategy(DataInspectionStrateg): The strategy to be us
+        Returns:
+            None 
+        """
+        self._strategy = strategy
+
+    def set_strategy(self, strategy: DataInspectionStrategy):
+        """
+        Sets a new strategy for the DataInspector
+
+        Args:
+            strategy(DataInspectionStrateg): The strategy to be us
+        
+        Returns:
+            None
+        """
+        self._strategy = strategy
+
+    def execute_inspection(self, df: pd.DataFrame):
+        """
+        Executes the inspection using the current strategy.
+
+        Args:
+            df(pd.DataFrame): dataframe to inspect
+
+        Returns: 
+            None: Executes the strategy's inspection method
+        """
+
+        self._strategy.inspect(df)
+
+
+
+# Example usage
+if __name__ == "__main__": 
+    # Example usage of the DataInspector with different strategies
+    # Load the data 
+    df = pd.read_csv("/home/ubuntu/mlops-projects/Prices-Priedictor-System/extracted_data/AmesHousing.csv")
+
+    # INITIALIZE THE DATA INSPECTOR WITH A SPECIFIC STRATEGY
+    inspector  = DataInspector(DataTypesInspectionStrategy())
+    inspector.execute_inspection(df)
+
+    # Change strategy to Summary Statistics and execute
+    inspector.set_strategy(SummaryStatisticsInspectionStrategy())
+    inspector.execute_inspection(df) 
--- explanations/strategy_design_pattern.py
+++ explanations/strategy_design_pattern.py
@@ -12,3 +12,35 @@ class PaymentMethod(ABC):
 class CreditCardPayment(PaymentMethod):
     def pay(self, amount):
         return f"Paying {amount} using Credit Card"
+    
+
+ 
+class PayPalPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Paying {amount} using PayPal"
+
+
+class BitcoinPayment(PaymentMethod):
+    def pay(self, amount):
+        return f"Paying {amount} using Bitcoin."
+
+
+# Step2: Implement the Context
+class ShoppingCart:
+    def __init__(self, payment_method: PaymentMethod):
+        self.payment_method = payment_method
+
+    def checkout(self, amount):
+        return self.payment_method.pay(amount)
+
+
+# Step4: Use the strategy in the Context
+if __name__ == "__main__":
+    cart = ShoppingCart(CreditCardPayment())
+    print(cart.checkout(1000))
+
+    cart = ShoppingCart(CreditCardPayment())
+    print(cart.checkout(2000))
+
+    cart = ShoppingCart(CreditCardPayment())
+    print(cart.checkout(3000))
